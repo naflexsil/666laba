e7c163328921856f17aacb3346175ddbfdaa451a
Starikova Alina <starrikovaalina@gmail.com>
2023-05-29 17:42:52 +0700

    a fly landed on jam - that's the whole poem

--- Program.cs
+++ Program.cs
@@ -1,1 +1,343 @@
 ﻿
+            /**********************************
+            *                                 *
+            *        Стариковой Алины         *
+            *       "Делегаты, события"       *
+            *                                 *
+            **********************************/
+
+using System;
+using System.Diagnostics;
+
+namespace laba666 {
+    public class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("\t\n||||||||||||| размер квадратной матрицы |||||||||||||");
+// М А Т Р И Ц А  3   
+            AboutMatrix MatrixA = new AboutMatrix();
+            MatrixA.Size = Convert.ToInt32(Console.ReadLine());
+            AboutMatrix MatrixB = new AboutMatrix();
+            MatrixB.Size = Convert.ToInt32(Console.ReadLine());
+            var RandomNumber = new Random((int)Stopwatch.GetTimestamp());
+            for (int IndexColumn = 0; IndexColumn < MatrixA.Size; ++IndexColumn) {
+                for (int IndexRow = 0; IndexRow < MatrixA.Size; ++IndexRow) {
+                    MatrixA.array[IndexColumn, IndexRow] = RandomNumber.Next(10, 100);
+                    MatrixB.array[IndexColumn, IndexRow] = RandomNumber.Next(10, 100);
+                }
+            }
+            Console.WriteLine("\n\n\n\t\t! красивенькие готовенькие матрицы !");
+            Console.WriteLine(MatrixA.ToString());
+            Console.WriteLine(MatrixB.ToString());
+            Console.WriteLine("\n\n\n\t||||||||||||| что желаем? |||||||||||||");
+            Console.WriteLine("1)  сложить\n2)  вычесть\n3)  умножить\n4)  разделить\n5)  проверить на равность\n6)  сравнить на >\n7)  сравнить на <\n8)  сравнить на >=\n9)  сравнить на <=\n10) найти определитель\n11) обратную матрицу\n12) перейти к матричному калькулятору\n");
+            string choice = Console.ReadLine();
+            switch (choice) {
+                case "1":
+                    Console.WriteLine($" 1 матрица + 2 матрица \n{MatrixA + MatrixB}");
+                    break;
+                case "2":
+                    Console.WriteLine($" 1 матрица - 2 матрица \n{MatrixA - MatrixB}");
+                    break;
+                case "3":
+                    Console.WriteLine($" 1 матрица * 2 матрица \n{MatrixA * MatrixB}");
+                    break;
+                case "4":
+                    Console.WriteLine($" 1 матрица / 2 матрица \n{MatrixA / MatrixB}");
+                    break;
+                case "5":
+                    Console.WriteLine($" 1 матрица == 2 матрица: {MatrixA == MatrixB}");
+                    break;
+                case "6":
+                    Console.WriteLine($" 1 матрица > 2 матрица: {MatrixA > MatrixB}");
+                    break;
+                case "7":
+                    Console.WriteLine($" 1 матрица < 2 матрица: {MatrixA < MatrixB}");
+                    break;
+                case "8":
+                    Console.WriteLine($" 1 матрица >= 2 матрица: {MatrixA >= MatrixB}");
+                    break;
+                case "9":
+                    Console.WriteLine($" 1 матрица <= 2 матрица: {MatrixA <= MatrixB}");
+                    break;
+                case "10":
+                    Console.WriteLine($" детерминант матрицы 1: {MatrixA.Determ(MatrixA)}");
+                    break;
+                case "11": {
+                        var inverseA = MatrixA.Inverse(MatrixA);
+                        Console.WriteLine($" инверсия матрицы:\n{inverseA}");
+                        break;
+                    }
+
+                case "12": {
+                        Console.Clear();
+                        Console.WriteLine($"||||||||||||| переход к матричному калькулятору |||||||||||||\n");
+                        var Transition = MatrixA.MatrixTransposition();
+                        Console.WriteLine("\n\t! транспонированная матрица !");
+                        Console.WriteLine(Transition);
+                        double trace = MatrixA.MatrixTrace();
+                        Console.WriteLine($"\n след матрицы = {trace}\n");
+                        ConvertToDiagonal(MatrixA);
+                        Console.WriteLine(MatrixA.ToString());
+                        break;
+                    }
+            }
+            void ConvertToDiagonal(AboutMatrix A) {
+                Action<AboutMatrix> convertDelegate = delegate (AboutMatrix matrix) {
+                    for (int Column = 0; Column < matrix.Size; ++Column) {
+                        for (int Row = 0; Row < matrix.Size; ++Row) {
+                            if (Column != Row)
+                                matrix.array[Column, Row] = 0;
+                        }
+                    }
+                };
+                A.ConvertToDiagonal(convertDelegate);
+                Console.WriteLine("\n\t! держи матрицу диагональную !");
+            }
+            Console.ReadKey();
+        }
+        public class AboutMatrix {
+            public int Size;
+            public double[,] array = new double[10, 10];
+            public void ConvertToDiagonal(Action<AboutMatrix> convertDelegate) {
+                convertDelegate(this);
+            }
+// D E E P  C O P Y
+            public AboutMatrix DeepCopy() {
+                AboutMatrix clone = (AboutMatrix)this.MemberwiseClone();
+                return clone;
+            }
+// C O M P A R E  T O
+            public int CompareTo(AboutMatrix other) {
+                if (other is null)
+                    return 1;
+                if (Size != other.Size)
+                    return Size.CompareTo(other.Size);
+                for (int IndexColumn = 0; IndexColumn < Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < Size; ++IndexRow) {
+                        int compare = array[IndexColumn, IndexRow].CompareTo(other.array[IndexColumn, IndexRow]);
+                        if (compare != 0)
+                            return compare;
+                    }
+                }
+                return 0;
+            }
+// E Q U A L S
+            public override bool Equals(object obj) {
+                if (obj is null || !(obj is AboutMatrix)) {
+                    return false;
+                }
+                return this == (AboutMatrix)obj;
+            }
+// G E T  H A S H  C O D E
+            public override int GetHashCode() {
+                unchecked {
+                    int hashCode = 20;
+                    for (int ColumnCounter = 0; ColumnCounter < Size; ++ColumnCounter) {
+                        for (int RowCounter = 0; RowCounter < Size; ++RowCounter) {
+                            hashCode = hashCode * 23 + array[ColumnCounter, RowCounter].GetHashCode();
+                        }
+                    }
+                    return hashCode;
+                }
+            }
+// С Л О Ж Е Н И Е
+            public static AboutMatrix operator + (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        A.array[IndexColumn, IndexRow] = A.array[IndexColumn, IndexRow] + B.array[IndexColumn, IndexRow];
+                    }
+                }
+                return A;
+            }
+// В Ы Ч И Т А Н И Е
+            public static AboutMatrix operator - (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        A.array[IndexColumn, IndexRow] = A.array[IndexColumn, IndexRow] - B.array[IndexColumn, IndexRow];
+                    }
+                }
+                return A;
+            }
+// У М Н О Ж Е Н И Е 
+            public static AboutMatrix operator * (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int Index = 0; Index < A.Size; ++Index) {
+                        for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                            A.array[IndexColumn, Index] += A.array[IndexRow, Index] * B.array[IndexColumn, IndexRow];
+                        }
+                    }
+                }
+                return A;
+            }
+// Д Е Л Е Н И Е
+            public static AboutMatrix operator / (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        try {
+                            A.array[IndexColumn, IndexRow] = A.array[IndexColumn, IndexRow] / B.array[IndexColumn, IndexRow];
+                        }
+                        catch {
+                            A.array[IndexColumn, IndexRow] = 0;
+                        }
+                    }
+                }
+                return A;
+            }
+// С Р А В Н Е Н И Я
+            public static bool operator == (AboutMatrix A, AboutMatrix B) {
+                if (A.Size != B.Size) {
+                    return true;
+                }
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        if (A.array[IndexColumn, IndexRow] == B.array[IndexColumn, IndexRow]) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            public static bool operator != (AboutMatrix A, AboutMatrix B) {
+                if (A.Size != B.Size) {
+                    return true;
+                }
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        if (A.array[IndexColumn, IndexRow] != B.array[IndexColumn, IndexRow]) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            public static AboutMatrix operator > (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        if (A.array[IndexColumn, IndexRow] > B.array[IndexColumn, IndexRow]) {
+                            A.array[IndexColumn, IndexRow] = 1;
+                        }
+                        else {
+                            A.array[IndexColumn, IndexRow] = 0;
+                        }
+                    }
+                }
+                return A;
+            }
+            public static AboutMatrix operator < (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        if (A.array[IndexColumn, IndexRow] < B.array[IndexColumn, IndexRow]) {
+                            A.array[IndexColumn, IndexRow] = 1;
+                        }
+                        else {
+                            A.array[IndexColumn, IndexRow] = 0;
+                        }
+                    }
+                }
+                return A;
+            }
+            public static AboutMatrix operator >= (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        if (A.array[IndexColumn, IndexRow] >= B.array[IndexColumn, IndexRow]) {
+                            A.array[IndexColumn, IndexRow] = 1;
+                        }
+                        else {
+                            A.array[IndexColumn, IndexRow] = 0;
+                        }
+                    }
+                }
+                return A;
+            }
+            public static AboutMatrix operator <= (AboutMatrix A, AboutMatrix B) {
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        if (A.array[IndexColumn, IndexRow] <= B.array[IndexColumn, IndexRow]) {
+                            A.array[IndexColumn, IndexRow] = 1;
+                        }
+                        else {
+                            A.array[IndexColumn, IndexRow] = 0;
+                        }
+                    }
+                }
+                return A;
+            }
+
+// М И Н О Р  +  О П Р Е Д Е Л И Т Е Л Ь
+            public static AboutMatrix Minor(AboutMatrix A, int Column, int Row) {
+                AboutMatrix buf = new AboutMatrix();
+                for (int IndexColumn = 0; IndexColumn < A.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < A.Size; ++IndexRow) {
+                        if ((IndexRow != Row) || (IndexColumn != Column)) {
+                            if (IndexColumn > Column && IndexRow < Row) buf.array[IndexColumn - 1, IndexRow] = A.array[IndexColumn, IndexRow];
+                            if (IndexColumn < Column && IndexRow > Row) buf.array[IndexColumn, IndexRow - 1] = A.array[IndexColumn, IndexRow];
+                            if (IndexColumn > Column && IndexRow > Row) buf.array[IndexColumn - 1, IndexRow - 1] = A.array[IndexColumn, IndexRow];
+                            if (IndexColumn < Column && IndexRow < Row) buf.array[IndexColumn, IndexRow] = A.array[IndexColumn, IndexRow];
+                        }
+                    }
+                }
+                return buf;
+            }
+            public double Determ(AboutMatrix A) {
+                double det = 0;
+                int Rank = A.Size;
+                if (Rank == 1) det = A.array[0, 0];
+                if (Rank == 2) det = A.array[0, 0] * A.array[1, 1] - A.array[0, 1] * A.array[1, 0];
+                if (Rank > 2) {
+                    for (int Index = 0; Index < A.Size; ++Index) {
+                        det += Math.Pow(-1, 0 + Index) * A.array[0, Index] * Determ(Minor(A, 0, Index));
+                    }
+                }
+                return det;
+            }
+            private AboutMatrix SubMatrix(int Row, int Column) {
+                var subMatrix = new AboutMatrix();
+                int subRow = 0;
+                for (int IndexRow = 0; IndexRow < Size; ++IndexRow) {
+                    if (IndexRow == Row)
+                        continue;
+                    int subColumn = 0;
+                    for (int IndexColumn = 0; IndexColumn < Size; ++IndexColumn) {
+                        if (IndexColumn == Column)
+                            continue;
+                        subMatrix.array[subColumn, subColumn] = array[IndexColumn, IndexColumn];
+                        ++subColumn;
+                    }
+                    ++subRow;
+                }
+                return subMatrix;
+            }
+
+// И Н В Е Р С И Я
+            public AboutMatrix Inverse(AboutMatrix A) {
+                var determinant = Determ(A);
+                if (determinant == 0) {
+                    throw new InvalidOperationException(" инверсии не может быть");
+                }
+                var Result = new AboutMatrix();
+                int sign = 1;
+                for (int IndexColumn = 0; IndexColumn < Result.Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < Result.Size; ++IndexRow) {
+                        var subMatrix = SubMatrix(IndexColumn, IndexRow);
+                        Result.array[IndexRow, IndexColumn] = sign * subMatrix.Determ(A) / determinant;
+                        sign = -sign;
+                    }
+                }
+                return Result;
+            }
+
+// В Ы В О Д  М А Т Р И Ц Ы
+            public override string ToString() {
+                string Result = $"\n размеры: {Size} x {Size}";
+                Result += $"\n|||||||||||||||||||||||||||||||||||||||\n" + "\t\tМатрица\n";
+                for (int IndexColumn = 0; IndexColumn < Size; ++IndexColumn) {
+                    for (int IndexRow = 0; IndexRow < Size; ++IndexRow) {
+                        Result += array[IndexColumn, IndexRow].ToString() + "\t";
+                    }
+                    Result += "\n";
+                }
+                return Result;
+            }
+        }
+    }
+}
